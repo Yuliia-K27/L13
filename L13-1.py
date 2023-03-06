09b07f014d1126b1325e46d127bd7b0d999675f6
Yuliia <121313266+Yuliia-K27@users.noreply.github.com>
2023-03-06 16:38:14 +0200

    add - sort_and_time (L13-2)

--- L13-1.py
+++ L13-1.py
@@ -12,3 +12,31 @@ for i in range(5000):
     int_list.append(random.randint(0, 1000))
     float_list.append(round(random.uniform(0.1, 100.0), 2))
     str_list.append(''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=random.randint(3, 10))))
+    
+import random
+import timeit
+
+int_list = []
+float_list = []
+str_list = []
+
+#щоб обрахувати середній час роботи алгоритму сортування, використаємо функцію timeit() з модуля timeit.
+#застосуємо функцію sort_and_time() до кожного з трьох списків int_list, float_list та str_list, та використаємо вбудований метод сортування Python sorted(). 
+#результат - середній час роботи алгоритму сортування для кожного списку, який виводиться на екран.
+def sort_and_time(list_to_sort, sort_func, num_iter):
+    total_time = 0
+    for i in range(num_iter):
+        start_time = timeit.default_timer()
+        sorted_list = sort_func(list_to_sort)
+        end_time = timeit.default_timer()
+        total_time += end_time - start_time
+    return total_time / num_iter
+
+int_sorted_time = sort_and_time(int_list, sorted, 100)
+float_sorted_time = sort_and_time(float_list, sorted, 100)
+str_sorted_time = sort_and_time(str_list, sorted, 100)
+
+print(f"Average time to sort int_list: {int_sorted_time:.5f} seconds")
+print(f"Average time to sort float_list: {float_sorted_time:.5f} seconds")
+print(f"Average time to sort str_list: {str_sorted_time:.5f} seconds")
+    
